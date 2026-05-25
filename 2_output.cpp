fb131b82044a06c37d8c09689cf990a8a692b5c3
Md. Asif Hasan <asifcse19@gmail.com>
2026-05-25 16:43:39 +0600

    Add files via upload

---
+++ 2_output.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    cout<<"cout is used to show output on the screen"<<endl;
+
+    cout<<"it is part of the iostream library"<<endl;
+
+    cout<<"cout is a object of the ostream class"<<endl;
+
+    cout<<"when cout is called,it shows the whole line inside the double quotes"<<endl;
+
+    cout<<"My name is Asif"<<endl;
+
+    cout<<"endl is used to end the line and move the cursor to the next line"<< endl;
+
+    cout<<"endl is also used for alternative to '\\n' for new line"<<endl;
+
+    return 0;
+}
